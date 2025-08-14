215a798a4a58353ed815fa357fa67bb80dc7d84e
pvibes18 <156800513+pvibes18@users.noreply.github.com>
2025-08-14 13:20:06 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAtIGyeurQwTB_4lNGdAlPhVjloEuRELOE",
   authDomain: "expense-tracker-js-8e788.firebaseapp.com",
@@ -8,4 +14,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
