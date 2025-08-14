1a5bbc3de943a6c1322b11c66f1c35421d19c909
pvibes18 <156800513+pvibes18@users.noreply.github.com>
2025-08-14 13:03:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAtIGyeurQwTB_4lNGdAlPhVjloEuRELOE",
+  authDomain: "expense-tracker-js-8e788.firebaseapp.com",
+  projectId: "expense-tracker-js-8e788",
+  storageBucket: "expense-tracker-js-8e788.firebasestorage.app",
+  messagingSenderId: "566517327288",
+  appId: "1:566517327288:web:8bd72c447f2760c8d91b4d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
